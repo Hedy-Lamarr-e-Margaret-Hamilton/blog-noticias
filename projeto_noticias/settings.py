b503a5c2d94f9400ec3b61d3280d82359a2c58c1
Andreia Ferreira <ferreiramartinsandreia@gmail.com>
2024-04-02 00:15:35 -0300

    [RESOLVENDO CONFLITOS] criacao da entidade do usuario, bem como a sua inserção no banco

--- projeto_noticias/settings.py
+++ projeto_noticias/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'base',
     
     'comentarios',
+    'usuario'
 ]
 
 MIDDLEWARE = [
--- projeto_noticias/urls.py
+++ projeto_noticias/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from base.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicio),
-    path('comentarios/', include('comentarios.urls', namespace='comentarios'))
+    path('comentarios/', include('comentarios.urls', namespace='comentarios')),
+    path('usuario/', include('usuario.urls', namespace='usuario'))
 ]
