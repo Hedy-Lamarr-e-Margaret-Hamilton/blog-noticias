792cb79d7bbd8987771f00839bd87e282acfe5b7
laryscampark <laryssascampark@gmail.com>
2024-04-05 18:49:22 -0300

    Ajuste das rotas

--- base/views.py
+++ base/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def inicio(request):
     return render(request, 'index.html')
 
+def pesquisa(request):
+    return render(request, 'pesquisa.html', {})
+
+def detalhes_noticia(request, id):
+    return HttpResponse(f"Detalhes da notícia com ID {id}")
+
+def noticias_por_categoria(request, categoria):
+    return HttpResponse(f"Notícias da categoria {categoria}")
+
+def autores(request):
+    return HttpResponse("Lista de autores de notícias")
+
+def noticias_por_autor(request, id):
+    return HttpResponse(f"Notícias do autor com ID {id}")
