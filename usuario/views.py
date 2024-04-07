188aefef4a5ae2c1a4f01a55a1a07008f5f04e29
[marcellaamazonas] <[marcellaamazonas@gmail.com]>
2024-04-07 11:14:50 -0300

    Funcionalidade de login

--- usuario/views.py
+++ usuario/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuario.models import User
-from usuario.forms import UserForm
+from usuario.forms import UserForm, UserLoginForm
 
 # Create your views here.
 def criar_user(request):
@@ -17,3 +17,13 @@ def criar_user(request):
     }
     
     return render(request, 'criar_usuario.html', contexto)
+
+def login(request):
+    form = UserLoginForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        if User.objects.filter(email=form.cleaned_data['email'], senha=form.cleaned_data['senha']).exists():
+            return redirect('noticias:criar_noticia')
+        else:
+            form.add_error(None, 'Usuário não encontrado.')
+    
+    return render(request, 'login.html', { 'form': form })
