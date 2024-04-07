c4bb1c0fdd9762a1c94692c756898da2a6510a52
[marcellaamazonas] <[marcellaamazonas@gmail.com]>
2024-04-07 18:20:28 -0300

    filtrando usuário pela permissão

--- usuario/views.py
+++ usuario/views.py
@@ -22,7 +22,12 @@ def login(request):
     form = UserLoginForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():
         if User.objects.filter(email=form.cleaned_data['email'], senha=form.cleaned_data['senha']).exists():
-            return redirect('noticias:criar_noticia')
+            usuario = User.objects.filter(email=form.cleaned_data['email'], senha=form.cleaned_data['senha']).first()
+            
+            if usuario.role == 'Autor':
+                return redirect('noticias:criar_noticia')
+            
+            return redirect('noticias:painel_noticias')
         else:
             form.add_error(None, 'Usuário não encontrado.')
     
